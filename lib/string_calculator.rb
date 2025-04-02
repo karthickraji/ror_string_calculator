9dde9f39d062d6124e41fa11e4454ff41642f7ec
karthickraji <karthickcool17@gmail.com>
2025-04-02 23:57:35 +0530

    Fourth commit: refactor string number calc

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,9 +2,11 @@ module StringCalculator
 
   def self.add(string_numbers)
     return 0 if string_numbers.empty?
-    splitted_num = string_numbers.split(",")
-    converted_to_integer = splitted_num.map{|x| x.to_i}
-    converted_to_integer.sum
+  end
+
+  def get_number_from_string(string_numbers)
+    numbers = string_numbers.split(",").map{|x| x.to_i}
+    numbers.sum
   end
 
 end
