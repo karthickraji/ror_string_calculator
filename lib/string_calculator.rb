297e79f800245dd7ac8e7f9b95ee4c30ca24a711
karthickraji <karthickcool17@gmail.com>
2025-04-03 00:00:28 +0530

    5th commit: fixed small bug

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,9 +2,10 @@ module StringCalculator
 
   def self.add(string_numbers)
     return 0 if string_numbers.empty?
+    get_number_from_string(string_numbers)
   end
 
-  def get_number_from_string(string_numbers)
+  def self.get_number_from_string(string_numbers)
     numbers = string_numbers.split(",").map{|x| x.to_i}
     numbers.sum
   end
